2e60471891bce404fcfa53bb3169e8fb3a6e2809
Alex Tester <lex.tester93@gmail.com>
2023-08-24 14:15:12 -0400

    Created func. to display main menu, created func. to handle user menu choice.

--- server.js
+++ server.js
@@ -21,3 +21,54 @@ connection.connect(err => {
   console.log("Welcome to the Dunder Mifflin employee tracker");
   startMenu();
 });
+// Function to display the main menu
+const startMenu = () => {
+  inquirer.prompt({
+    message: 'What would you like to do today?',
+    name: 'menu',
+    type: 'list',
+    choices: [
+      'View all departments',
+      'View all jobs',
+      'View all employees',
+      'Add a department',
+      'Add a job',
+      'Add an employee',
+      'Update employee job',
+      'Exit',
+    ],
+  })
+  .then(handleMenuChoice);
+};
+
+// Function to handle user menu choice
+const handleMenuChoice = response => {
+  switch (response.menu) {
+    case 'View all departments':
+      viewDepartment();
+      break;
+    case 'View all jobs':
+      viewJobs();
+      break;
+    case 'View all employees':
+      viewEmployees();
+      break;
+    case 'Add a department':
+      addDepartment();
+      break;
+    case 'Add a job':
+      addJob();
+      break;
+    case 'Add an employee':
+      addEmployee();
+      break;
+    case 'Update employee job':
+      updateEmployee();
+      break;
+    case "Exit":
+      connection.end();
+      break;
+    default:
+      connection.end();
+  }
+};
